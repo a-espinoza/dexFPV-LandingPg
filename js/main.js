9ff1b930c7503aa601985ef18285d5423ebe1a84
YOUR FULL NAMEAndres <a_espinosa@live.com>
2017-02-25 22:49:56 -0500

    added js for insta

--- js/main.js
+++ js/main.js
@@ -1,72 +1,15 @@
-console.log("im working")
-
-var ig = {};
-// !!! USE YOUR OWN TOKEN
-ig.token = '189242245.e4579bd.785b15a243fe4d87918dc5982b136bbd';
-
-ig.init = function() {
-  $('.instagram').each(function(i) {
-    var args = {};
-    args.container = $(this);
-    args.userid = args.container.data('userid');
-    args.limit = args.container.data('limit');
-    args.feedurl = 'https://api.instagram.com/v1/users/' + args.userid + '/media/recent/?access_token=' + ig.token + '&count=' + args.limit + '&callback=?';
-    args.html = '';
-    // PASS ARGS TO QUERY
-    ig.query(args);
+$(function(){
+  var accessToken = '3696661727.1677ed0.95bfb6027efb481896579c0039e692c7';
+  $.getJSON('https://api.instagram.com/v1/users/self/media/recent/?access_token='+accessToken+'&callback=?',function (insta) {
+    $.each(insta.data,function (photos,src) {
+      if ( photos === 5 ) { return false; }
+      $('<a href="'+src.link+'" class="post">'+
+        '<div class="image" style="background-image:url('+src.images.standard_resolution.url+');"></div>'+
+        '<ul>'+
+        '<li><i class="fa fa-camera"></i> '+src.filter+'</li>'+
+        '<li><i class="fa fa-heart"></i> '+src.likes.count+'</li>'+
+        '<li><i class="fa fa-comment"></i> '+src.comments.count+'</li>'+
+        '</ul></a>').appendTo('#content');
+    });
   });
-}
-
-ig.query = function(args) {
-  $.getJSON(args.feedurl, {}, function(data) {
-    // PASS QUERY DATA TO BUILDER
-    ig.build(data, args);
-  });
-}
-
-ig.build = function(data, args) {
-
-  $.each(data.data, function(i, item) {
-    console.log(item);
-    if (item.caption) var caption = item.caption.text;
-    var thumb = item.images.low_resolution.url;
-    var img = item.images.standard_resolution.url;
-    //get 1280 size photo [hack until avail in api]
-    var hires = img.replace('s640x640', '1080x1080');
-    args.html += '<a class="image" style="background-image: url(' + thumb + ');" data-img="' + hires + '">';
-    if (caption) args.html += '<span class="caption">' + caption + '</span>';
-    args.html += '</a>';
-    // PASS TO OUTPUT
-    ig.output(args);
-  });
-}
-
-ig.output = function(args) {
-  args.container.html(args.html);
-}
-
-ig.view = {
-  viewer: $('.igviewer'),
-  image: $('.igviewer img'),
-  open: function(img) {
-    ig.view.viewer.removeClass('hidden');
-    ig.view.image.attr('src', img);
-  },
-  close: function() {
-    ig.view.viewer.addClass('hidden');
-    ig.view.image.attr('src', '');
-  }
-}
-
-ig.init();
-
-//Listeners
-$('.instagram').on('click', '.image', function() {
-  var img = this.dataset.img;
-  ig.view.open(img);
 });
-$('.igviewer').on('click', function() {
-  ig.view.close();
-});
-
-console.log("im stil working")
